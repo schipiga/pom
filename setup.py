beda16fcb35b2560ccf08aa3c98eb7f8f8b0ae54
Sergei Chipiga <sergei.chipiga@pipedrive.com>
2021-06-08 18:05:36 +0300

    v1.0.6

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-pom',
-    version='1.0.5',
+    version='1.0.6',
     description=('POM is Page-Object-Model microframework to develop web UI '
                  'tests easy, quickly and with pleasure.'),
     long_description=open('readme.rst').read(),
@@ -31,8 +31,8 @@ setup(
     packages=find_packages(),
     url='https://github.com/sergeychipiga/pom',
     install_requires=[
-        'selenium==3.3.0',
-        'waiting==1.3.0',
-        'six==1.11.0',
+        'selenium==3.141.0',
+        'waiting==1.4.1',
+        'six==1.14.0',
     ]
 )
